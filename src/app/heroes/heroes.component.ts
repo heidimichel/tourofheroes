ff305c90bc2697c5e195e73cdeede564f95186d9
heidimichel <heidi_michel@hotmail.com>
2026-02-20 21:42:14 +0100

    Add cute styling + add service + use observables

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
+import { Observable, of } from 'rxjs';
+import { HEROES } from '../mock-heroes';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,19 +16,22 @@ import { HeroService } from '../hero.service';
   styleUrl: './heroes.component.scss'
 })
 export class HeroesComponent implements OnInit {
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) { } // in angular 20 and above, you can also use: private heroService = inject(HeroService);
   heroes: Hero[] = [];
   selectedHero?: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
 
   ngOnInit(): void {
     this.getHeroes();
   }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
 }
